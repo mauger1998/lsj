7e2573c880356c52ed9c6bdadb8bfd1f37f0d460
Muhammad-Naiem <80773177+Muhammad-Naiem@users.noreply.github.com>
2023-09-27 16:30:22 +0600

    Update-index.js

--- job-board/js/index.js
+++ job-board/js/index.js
@@ -9,6 +9,7 @@
 //     }
 // })
 
+
 let URLTHREE =
     'https://1r3pn5o9.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type+%3D%3D+%27vacancies%27%5D%7B%0AjobTitle%2C%0AstartDate%2C%0Aduration%2C%0AcompanyName%2C%0Askills%2C%0Adescription%0A%7D&perspective=published'
 // fetch the content
